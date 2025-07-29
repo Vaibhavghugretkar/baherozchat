63220352dd48db55f0403327fe483c64d14de244
Vaibhavghugretkar <your-email@example.com>
2025-07-29 10:15:07 +0530

    last fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5173",
-      "https://baherozchat.netify.app",
+      "https://baherozchat.netlify.app",
     ],
 
     credentials: true,
