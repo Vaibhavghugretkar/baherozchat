a2b8fcdbb5f7f729d4dea6a0f8fb00bbb8a874e4
Vaibhavghugretkar <your-email@example.com>
2025-07-29 09:48:49 +0530

    changes url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://baherozchat.onrender.com/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://baherozchat.onrender.com/api" : "https://baherozchat.onrender.com/api",
   withCredentials: true,
 });
