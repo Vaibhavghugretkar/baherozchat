6387fb0d6a4ed81c7fbd7147b6a20031c2280d9b
Vaibhavghugretkar <your-email@example.com>
2025-07-29 09:45:24 +0530

    fixed url issue

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://baherozchat.onrender.com" : "https://baherozchat.onrender.com/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://baherozchat.onrender.com" : "https://baherozchat.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
